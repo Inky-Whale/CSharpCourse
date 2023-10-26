9004d466239dfad3891f279f2505d52ecd407193
Inky-Whale <berk.karanfiloglu@gmail.com>
2023-10-26 20:12:05 +0300

    m

---
+++ Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Classes;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Update();
+
+        Console.WriteLine();
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add();
+        productManager.Update();
+
+        Customer customer = new Customer();
+        customer.City = "Ankara";
+        customer.Id = 1;
+        customer.FirstName = "Berk";
+        customer.LastName = "Karanfiloglu";
+
+        Customer customer2 = new Customer
+        {
+            FirstName = "Berk",
+            LastName = "Karanfiloglu",
+            Id = 2,
+            City = "Istanbul"
+        };
+        Console.WriteLine();
+
+        Console.WriteLine(customer2.FirstName);
+
+        Console.ReadLine();
+    }
+}
+
